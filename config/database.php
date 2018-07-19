e932f4349ff1ded9cd5bd765db18b52e746fb5cd
Eddy Ganz <eddienyagano@gmail.com>
2018-07-19 20:57:25 +0200

    added database
    added database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'enyagano_dreamOn'),
+            'username' => env('DB_USERNAME', 'nyagano_eddy'),
+            'password' => env('DB_PASSWORD', 'Eddy2020#'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
