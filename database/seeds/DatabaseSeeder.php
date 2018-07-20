91f7d4825b97a291e80989be4fe73d6f16f63cc0
Eddy Ganz <eddienyagano@gmail.com>
2018-07-20 18:39:18 +0200

    php artisan db:seed
    php artisan db:seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker\Factory::create();
+
+        $limit = 33;
+
+        for ($i = 0; $i < $limit; $i++) {
+            // $this->call(UsersTableSeeder::class);
+            DB::table('dreams')->insert([
+                'heading' => $faker->title,
+                'description' =>$faker->text,
+                'tags' => $faker->text,
+                'important_facts' => $faker->text,
+            ]);
+        }
+
     }
 }
