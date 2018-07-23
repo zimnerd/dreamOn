41a71af809495c1f6829c034ffefc49e69b98ffe
Eddy Ganz <eddienyagano@gmail.com>
2018-07-23 23:49:08 +0200

    edit table

--- database/migrations/2018_07_20_075606_create_dreams_table.php
+++ database/migrations/2018_07_20_075606_create_dreams_table.php
@@ -16,7 +16,7 @@ class CreateDreamsTable extends Migration
         Schema::create('dreams', function (Blueprint $table) {
             $table->increments('id');
             $table->string('heading');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->after('id');
             $table->text('description');
             $table->string('tags');
             $table->string('important_facts');
