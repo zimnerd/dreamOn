fca148c4009e02cf8a1fae67ce67e192c7ddc373
Eddy Ganz <eddienyagano@gmail.com>
2018-07-30 09:24:07 +0200

    headers

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-header ("Access-Control-Allow-Origin: *");
-header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
-header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
-header ("Access-Control-Allow-Headers: Origin, Content-Type, Content-Range, Authorisation, Accept, X-Requested-With,Content-Disposition, Content-Description, X-Auth-Token");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Expose-Headers: Content-Length, X-JSON");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, Content-Range, Authorisation, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Accept, X-Requested-With,Content-Disposition, Content-Description, X-Auth-Token");
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
@@ -17,8 +17,8 @@ $uri = urldecode(
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
     return false;
 }
 
-require_once __DIR__.'/public/index.php';
+require_once __DIR__ . '/public/index.php';
