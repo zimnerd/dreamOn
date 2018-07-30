4b192c0b0495f11c17b9fd48480b815c0e187cd6
Eddy Ganz <eddienyagano@gmail.com>
2018-07-30 08:33:55 +0200

    logging

---
+++ app/Http/Middleware/LogAfterRequest.php
@@ -0,0 +1,17 @@
+use Closure;
+use Illuminate\Contracts\Routing\TerminableMiddleware;
+use Illuminate\Support\Facades\Log;
+
+class LogAfterRequest implements TerminableMiddleware {
+
+public function handle($request, Closure $next)
+{
+return $next($request);
+}
+
+public function terminate($request, $response)
+{
+Log::info('app.requests', ['request' => $request->all(), 'response' => $response]);
+}
+
+}
